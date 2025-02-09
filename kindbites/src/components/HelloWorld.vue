fd249e1b6376e6edd16e86ff7c08a48a6fbb948f
ibrahimkaram <87280420+ibrahimkaram@users.noreply.github.com>
2025-02-09 15:25:06 -0500

    Add Initial files setup for the project
    Using Vite and Vue3js

---
+++ kindbites/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+  
+  </div>
+
+  <p>
+    Kind Bites is a food rescue platform that will connect businesses with a surplus of food to nonprofit organizations or individuals in need
+  </p>
+  <p>
+    Comming soon
+  </p>
+  <p class="read-the-docs">Project is under development</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
